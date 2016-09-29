6cb46dfb4929786362a1d7d89f87c215eb6ff91d
Kiran Rao <kiran_rao@outlook.com>
2016-09-29 00:08:09 -0400

    Add sigmoid functions and tests

---
+++ sigmoid.py
@@ -0,0 +1,18 @@
+import numpy as np
+
+def sigmoid(z):
+    return 1/(1+np.exp(-z))
+
+def sigmoid_d(z):
+    return np.exp(-z) / ( np.square((np.exp(-z) + 1)) )
+
+if __name__ == "__main__":
+    print('Sigmoid Tests')
+    testVector = np.matrix("1 0 -1")
+
+    for i in range(2, 5):
+        epsilon = 10 ** (-i)
+        print("Epsilon: ", epsilon)
+        testDerivative = (sigmoid(testVector + epsilon) - sigmoid(testVector)) / epsilon
+        realDerivative = sigmoid_d(testVector)
+        print(realDerivative - testDerivative)
