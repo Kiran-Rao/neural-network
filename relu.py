aa37e9b64afacde7a55162729208bb638ee7f468
Kiran Rao <kiran_rao@outlook.com>
2016-09-29 00:27:31 -0400

    Add Rectified Linear Unit Functions

---
+++ relu.py
@@ -0,0 +1,16 @@
+import numpy as np
+
+
+def relu(z):
+    return np.maximum(z, 0)
+
+def relu_d(z):
+    return np.sign(np.maximum(z, 0))
+
+if __name__ == "__main__":
+    print('ReLU Tests')
+    testVector = np.matrix("5 1 0 -1 -2")
+
+    print(testVector)
+    print(relu(testVector))
+    print(relu_d(testVector))
