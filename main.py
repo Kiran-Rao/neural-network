985292e5c95d4fc9748ec1a89cb778217b9b9faa
Kiran Rao <kiran_rao@outlook.com>
2016-10-02 00:26:05 -0400

    Add backprop algorithm

--- main.py
+++ main.py
@@ -7,8 +7,8 @@ class Neural_Net(object):
         self.inputLayerSize = 2
         self.outputLayerSize = 1
         self.hiddenLayerSize = 3
-        self.batchSize = 10
-        self.learningRate = 0
+        # self.batchSize = 10
+        # self.learningRate = 0
 
         # Weights
         self.weights1 = np.random.randn(self.inputLayerSize, self.hiddenLayerSize)
@@ -19,14 +19,40 @@ class Neural_Net(object):
         self.z2 = np.dot(X, self.weights1)
         self.a2 = sigmoid.sigmoid(self.z2)
         self.z3 = np.dot(self.a2, self.weights2)
-        return self.z3
+        yHat = sigmoid.sigmoid(self.z3)
+        return yHat
 
     def backprop(self, X):
         # Back Propegation
         pass
 
+    def cost(self, X, y):
+        # Cost function based on mean square errors
+        yHat = self.forward(X)
+        return np.mean(np.square(y - yHat)) / 2
+
+    def cost_d(self, X, y):
+        # Partial derivative of cost fuction wrt weight 1 and weight 2
+        self.yHat = self.forward(X)
+
+        # Calculate partial derivative wrt weight 2
+        delta3 = np.multiply( -(y - self.yHat), sigmoid.sigmoid_d(self.z3))
+        dJdW2 = np.dot(np.transpose(self.a2), delta3)
+
+        # Calculate partial derivative wrt weight 1
+        delta2 = np.dot(delta3, np.transpose(self.weights2)) * sigmoid.sigmoid_d(self.z2)
+        dJdW1 = np.dot(np.transpose(X), delta2)
+
+        print('Partial Derivatives')
+        print(dJdW1, dJdW2)
+        return dJdW1, dJdW2
+
 if __name__ == "__main__":
     nn = Neural_Net()
-    print ("Weights: ")
-    print (nn.weights1)
-    print (nn.weights2)
+    print("Weights: ")
+    print(nn.weights1)
+    print(nn.weights2)
+    x = [[1, 0.6], [0.6, 0.2], [0.4, 0.8]]
+    y = [[0.6], [0.2], [0.8]]
+    nn.cost(x, y)
+    nn.cost_d(x, y)
